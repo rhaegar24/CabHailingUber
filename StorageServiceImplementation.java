64c9ea84a6dece3deae4d55be37ae0d0b43e092b
Anadi Tiwari <54220368+rhaegar24@users.noreply.github.com>
2021-09-30 21:36:37 +0530

    Update StorageServiceImplementation.java

--- StorageServiceImplementation.java
+++ StorageServiceImplementation.java
@@ -1,36 +1,34 @@
 import java.util.*;
 public class StorageServiceImplementation implements lStorageService{
     
-    StorageServiceImplementation(){
-     HashMap<String, Rider> riderMap = new HashMap<>();
-     HashMap<String, Driver> driverMap = new HashMap<>();
-     HashMap<String, Vehicle> vehicleMap = new HashMap<>();
-     HashMap<String, Booking> bookingMap = new HashMap<>();
-     List<Booking> bookList = new ArrayList<>();
-    }
+    HashMap<String, Rider> riderMap = new HashMap<>();
+    HashMap<String, Driver> driverMap = new HashMap<>();
+    HashMap<String, Vehicle> vehicleMap = new HashMap<>();
+    HashMap<String, Booking> bookingMap = new HashMap<>();
+    List<Booking> bookList = new ArrayList<>();
     
-    boolean saveRider(Rider rider){
-        if(!riderMap.containsKey(rider.getName()))
-          riderMap.put(rider.getName(), rider);                                            //What to do when it contains?
+    public boolean saveRider(Rider rider){
+        if(!riderMap.containsKey(rider.getName()))                                     //What to do when it contains?
+          riderMap.put(rider.getName(), rider);
         else return false;
     return true;
     }
     
-    boolean saveDriver(Driver driver){
-        if(!driverMap.containsKey(driver.getName()))
-          driverMap.put(driver.getName(), driver);                                        //What to do when it contains?
-        else return false;
+    public boolean saveDriver(Driver driver){
+        if(!driverMap.containsKey(driver.getName()))                                   //What to do when it contains?
+          driverMap.put(driver.getName(), driver);
+        else return false; 
     return true;
     }
     
-    boolean saveVehicle(Vehicle vehicle){
+    public boolean saveVehicle(Vehicle vehicle){
         if(!vehicleMap.containsKey(vehicle.getCarNumber()))
           vehicleMap.put(vehicle.getCarNumber(), vehicle);
         else return false;
     return true;
     }
     
-    boolean updateLocation(Vehicle vehicle){
+    public boolean updateLocation(Vehicle vehicle){
         String carNum = vehicle.getCarNumber();
         if(vehicleMap.containsKey(carNum))
             vehicleMap.put(carNum, vehicle);
@@ -38,23 +36,23 @@ public class StorageServiceImplementation implements lStorageService{
     return true;
     }
     
-    boolean book(Booking booking){
+    public boolean book(Booking booking){
         if(!bookingMap.containsKey(booking.getBookingId()))
           bookingMap.put(booking.getBookingId(), booking);
         else return false;
     return true;
     }
     
-    Vehicle find(Double lat, Double lon, Double maxDistance){                          //maxDistance?
+    public Vehicle find(Double lat, Double lon, Double maxDistance){                   //maxDistance?
         for(String carNum : vehicleMap.keySet()){                                      //O(n)
             Vehicle vTemp = vehicleMap.get(carNum); 
-            if(vTemp.getLat() == lat && vTemp.getLon() == lon)
+            if(vTemp.getLat() == lat && vTemp.getLat() == lon)
               return vTemp;
         }
     return null;
     }
     
-    List<Booking> rideHistory(String riderUserId){
+    public List<Booking> rideHistory(String riderUserId){
         for(String id : bookingMap.keySet()){                                           //O(n)
            Booking temp = bookingMap.get(id);                                           
            if(temp.getUserId().equals(riderUserId)){
@@ -65,7 +63,7 @@ public class StorageServiceImplementation implements lStorageService{
     return bookList;
     }
     
-    Boolean endTrip(Long timeStamp, String bookingId){
+    public Boolean endTrip(Long timeStamp, String bookingId){
         if(bookingMap.containsKey(bookingId)){
             Booking temp = bookingMap.get(bookingId);
             temp.setEndTime(timeStamp);
@@ -73,5 +71,5 @@ public class StorageServiceImplementation implements lStorageService{
             return true;
         }
     return false;
-    }                                                                               //All actions except the rideHistory() and find() are O(1)
+    }
 }
